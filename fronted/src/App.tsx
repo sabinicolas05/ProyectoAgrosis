8ecea20c954ec7da513d7b590b99e0bdd49eebd1
sabinicolas05 <nsabirojas@gmail.com>
2025-03-22 08:18:50 -0500

    cree la estructura y todo el inicio del proyecto  toda la interfaz grafica

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,21 +1,56 @@
-import { Route, Routes } from "react-router-dom";
-
+import React from "react";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
 import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 
+{/*  TRAZABILIDAD */}
+
+import RegisterForm from "./pages/register";
+
+
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <Routes>
+        {/* 🔹 Login sin Navbar */}
+        
+
+        {/* 🔹 Rutas protegidas con Navbar */}
+        <Route
+          element={
+              <Outlet />
+          }
+        >
+          <Route element={<RegisterForm />} path="/register" />
+
+          <Route element={<UsuariosList />} path="/usuarios" />
+          <Route element={<EditarUsuarioModal />} path="/usuarios/editar/:id" />
+          <Route element={<RegisterUserModal/>} path="/usuarios/editar/:id" />
+          <Route element={<PricingPage />} path="/pricing" />
+          <Route element={<BlogPage />} path="/blog" />
+          <Route element={<AboutPage />} path="/about" />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
+
 export default App;
+
---
+++ fronted/src/types/sensorTypes.ts
@@ -0,0 +1,8 @@
+export interface Sensor {
+  id: number;
+  fk_bancal: number;
+  fk_tipo_sensor: number;
+  fk_configuracion: number;
+  fk_cultivo: number;
+  medicion: number;
+}
