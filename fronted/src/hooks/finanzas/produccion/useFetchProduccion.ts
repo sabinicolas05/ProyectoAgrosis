002793b24aa11571d0b064158d5d380faf9489a8
sabinicolas05 <nsabirojas@gmail.com>
2025-03-22 18:39:19 -0500

    termine los hooks de produccion

---
+++ fronted/src/hooks/finanzas/produccion/useFetchProduccion.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+
+// Función para obtener producciones
+const fetchProduccion = async () => {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch("http://127.0.0.1:8000/api/produccion/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al obtener producciones");
+  }
+
+  return response.json();
+};
+
+// Hook personalizado para usar la query
+export const useFetchProduccion = () => {
+  return useQuery({
+    queryKey: ["producciones"],
+    queryFn: fetchProduccion,
+    staleTime: 5000, 
+    refetchInterval: 5000, 
+
+  });
+};
