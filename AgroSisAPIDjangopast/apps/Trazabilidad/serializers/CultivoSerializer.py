7924a97ac9f5193e2f29a79bfc57501a2187bc3a
juancc9 <sicsound00@gmail.com>
2025-03-23 18:42:19 -0500

    "Serializers necesarios para el map de campos foraneos"

--- AgroSisAPIDjangopast/apps/Trazabilidad/serializers/CultivoSerializer.py
+++ AgroSisAPIDjangopast/apps/Trazabilidad/serializers/CultivoSerializer.py
@@ -1,7 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from ..models.Cultivo import Cultivo
 
-class CultivoSerializer(ModelSerializer):
+class CultivoSerializer(serializers.ModelSerializer):
+    fk_especie_nombre = serializers.CharField(source="fk_especie.nombre", read_only=True)
+    fk_semillero_nombre = serializers.CharField(source="fk_semillero.nombre_semilla", read_only=True)
+
     class Meta:
         model = Cultivo
-        fields = '__all__'
+        fields = [
+            'id', 'nombre', 'descripcion', 'cantidad', 'fecha_siembra', 
+            'fk_especie', 'fk_especie_nombre', 'fk_semillero', 'fk_semillero_nombre'
+        ]
--- AgroSisAPIDjangopast/apps/Trazabilidad/serializers/PlagaSerializer.py
+++ AgroSisAPIDjangopast/apps/Trazabilidad/serializers/PlagaSerializer.py
@@ -1,7 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from ..models.Plaga import Plaga
 
-class PlagaSerializer(ModelSerializer):
+class PlagaSerializer(serializers.ModelSerializer):
+    fk_tipo_plaga_tipo = serializers.CharField(source="fk_tipo_plaga.tipo", read_only=True)  # Esto accede al campo 'tipo' del modelo Tipo_Plaga
+
     class Meta:
         model = Plaga
-        fields = '__all__'
+        fields = [
+            'id', 'nombre', 'fk_tipo_plaga', 'fk_tipo_plaga_tipo'  # Incluye tanto el ID de fk_tipo_plaga como el campo 'tipo'
+        ]
